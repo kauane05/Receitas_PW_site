3962837ec1ecd866966691908611fce9dc5ac7c8
Kauane <kauane.izidoro23@gmail.com>
2023-11-27 11:19:42 -0400

    Login

--- php/conexao.php
+++ php/conexao.php
@@ -11,4 +11,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
+
 ?>
--- php/login.php
+++ php/login.php
@@ -1,58 +1,28 @@
 <?php
-$_conn = mysqli_connect('127.0.0.1','root','','bd_site');
+$_conn = mysqli_connect('127.0.0.1','root','','bd_site_pw2');
 if($_conn===FALSE) {
  echo "Não foi possível conectar ao Servidor de banco de dados ";
 } else {
  echo "Foi possível conectar ao Servidor de banco de dados ";
 
- // Verificar se está chegando dados por POST
-if (!empty($_POST)) {
-  // Iniciar SESSAO (session_start)
-  session_start();
-  try {
-    // Montar a SQL
-    $sql = "SELECT nome, email FROM usuario WHERE email = :email AND senha = :senha";
+ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    // Preparar a SQL (pdo)
-    $stmt = $pdo->prepare($sql);
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+   
 
-    // Definir/Organizar os dados p/ SQL
-    $dados = array(
-      ':email' => $_POST['email'],
-      ':senha' => md5($_POST['senha'])
-    );
+    $sql = "INSERT INTO login (nome_log, email_log, senha_log) VALUES ('$nome', ' $email', '$senha')";
 
-    $stmt->execute($dados);
-    //$stmt->execute(array(':email' => $_POST['email'], ':senha' => $_POST['senha']));
-
-    $result = $stmt->fetchAll();
-
-    if ($stmt->rowCount() == 1) { // Se o resultado da consulta SQL trouxer um registro
-      // Autenticação foi realizada com sucesso
-
-      $result = $result[0];
-      // Definir as variáveis de sessão
-      $_SESSION['nome'] = $result['nome'];
-      $_SESSION['email'] = $result['email'];
-      
-
-      // Redirecionar p/ página inicial (ambiente logado)
-      header("Location: sugestoesDoDia.html");
-
-    } else { // Signifca que o resultado da consulta SQL não trouxe nenhum registro
-      // Falha na autenticaçao
-      // Destruir a SESSAO
-      session_destroy();
-      // Redirecionar p/ página inicial (login)
-      header("Location: index.html?msgErro=E-mail e/ou Senha inválido(s).");
-    }
-  } catch (PDOException $e) {
-    die($e->getMessage());
-  }
+    if ($_conn->query($sql) === TRUE) {
+        echo "Receita cadastrada com sucesso!";
+    } else {
+        echo "Erro ao cadastrar a receita: " . $_conn->error;
+    } 
+    $_conn->close();
 }
-else {
-  header("Location: index.html?msgErro=Você não tem permissão para acessar esta página..");
+else{
+    echo "erro de dados";
 }
-die();
 }
 ?>
